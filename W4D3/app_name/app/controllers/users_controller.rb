5b9f0bac5a6f462e5643356951ab7e2716b0bc99
Heiman Chan <heimanchan0@gmail.com>
2019-01-29 23:59:45 -0800

    Add ruby auth homework

--- W4D3/app_name/app/controllers/users_controller.rb
+++ W4D3/app_name/app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
-class UsersController < ApplicationContorller
+class UsersController < ApplicationController
+  before_action :require_current_user!, except: [:create, :new]
+  def create
+    @user = User.new(params[:user])
 
-  
+    if @user.save
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  private
+
+  def show
+    @user = User.find(params[:username])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
